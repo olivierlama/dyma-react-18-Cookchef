df03401be3b96b4f02603cd35217d79eb5378b34
olivierlama <olivier.lama@gmail.com>
2022-10-02 00:11:51 +0200

    page 26

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,19 +1,22 @@
 import styles from "./Content.module.scss";
+import Recipe from "./Recipe";
 function Content() {
   return (
     <div className="flex-fill container p-20">
       <h1 className="my-30">Découvrez n}s nouvelles recettes</h1>
       <div className={`card p-20 ${styles.contentCard}`}>
         <div className={styles.grid}>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
-          <div className={styles.elem}>ELEMENT</div>
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
+          <Recipe />
         </div>
       </div>
     </div>
